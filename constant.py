843ef9ceb2f5807896dff8a4034a04bdb95e3268
said-ml <113719075+said-ml@users.noreply.github.com>
2024-08-25 06:00:31 +0000

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,18 @@
+
+DATA_DIR:str="/content/drive/MyDrive/isic-2024-challenge.zip"             # ===> if you run the code in Google Colab
+#r'C:\Users\Setup Game\Music\Favorites\Downloads\isic-2024-challenge.zip' # ===> if you run the code locally
+# load the data from the url:https://www.kaggle.com/competitions/isic-2024-challenge/data
+
+train_csv_path:str='train-metadata.csv'
+test_csv_path:str='test-metadata.csv'
+sample_submission_path:str="sample_submission.csv"
+train_images_hdf5_path:str ='train-image.hdf5'
+test_images_hdf5_path:str  ='test-image.hdf5'
+train_images_path:str= 'train-image/image'
+
+IMAGE_SIZE: int = 224
+IMAGE_FORMAT: str = 'jpg'
+
+
+
+
